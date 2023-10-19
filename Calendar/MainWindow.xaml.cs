9513be65eef7d80e713d93444c3a8d66c56ca702
liulun <xland@live.cn>
2023-10-19 16:19:24 +0800

    Win title Btns

--- Calendar/MainWindow.xaml.cs
+++ Calendar/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +21,47 @@ namespace Calendar
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rect[] captionArea = new Rect[2] { new Rect(),new Rect()};
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var source = PresentationSource.FromVisual(this) as HwndSource;
+            source?.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == 0x0084)  //WM_NCHITTEST
+            {
+                var x = lParam.ToInt32() & 0xFFFF;
+                var y = lParam.ToInt32() >> 16;
+                for (int i = 0; i < captionArea.Length; i++)
+                {
+                    if (captionArea[i].Contains(x, y))
+                    {
+                        handled = true;
+                        return new IntPtr(2);
+                    }
+                }                
+            }
+            else if(msg == 0x0047) //WM_WINDOWPOSCHANGED
+            {
+                if(PresentationSource.FromVisual(this) != null)
+                {
+                    var p1 = this.PointToScreen(new Point());
+                    var p2 = this.PointToScreen(new Point(this.Width - 126, 0)); //126 winTitleBtns Width
+                    captionArea[0].X = p1.X;
+                    captionArea[0].Y = p1.Y;
+                    captionArea[0].Width = p2.X - p1.X;
+                    captionArea[0].Height = 38;
+                }
+            }
+            return IntPtr.Zero;
         }
     }
 }
---
+++ Calendar/WinTitleBtns.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    /// <summary>
+    /// WinTitleBtns.xaml 的交互逻辑
+    /// </summary>
+    public partial class WinTitleBtns : UserControl
+    {
+        public WinTitleBtns()
+        {
+            InitializeComponent();
+        }
+
+        private void minimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void maximizeRestoreBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (maximizeRestoreBtn.Content.ToString() == "\ue6e5")
+            {
+                var a1 = 1;
+            }
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+    }
+}
