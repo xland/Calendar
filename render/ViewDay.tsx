ab3fd110715a75f74eaa6d743cd9575948f88e36
liulun <xland@live.cn>
2023-10-23 13:33:16 +0800

    Update ViewDay.tsx

--- render/ViewDay.tsx
+++ render/ViewDay.tsx
@@ -1,7 +1,7 @@
-import React from "jsx-dom";
+import React, { MouseEventHandler } from "jsx-dom";
 import "./ViewDay.scss";
 export default function () {   
-    let addNewJob = (e:PointerEvent)=>{
+    let addNewJob = (e:any)=>{
         let target = e.target as HTMLElement;
         if(target.parentElement?.getAttribute("id") != "ViewDay" && !target.classList.contains("hourTag")){
             return;
