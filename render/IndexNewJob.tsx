c13efb70f1d88f7eadbf5b9b8e2f2b290da701c4
unknown <liuxiaolun@hikvision.com>
2023-11-13 10:56:46 +0800

    new job

--- render/IndexNewJob.tsx
+++ render/IndexNewJob.tsx
@@ -3,18 +3,38 @@ import "./IndexNewJob.scss";
 import TitleBarBtns from "./TitleBarBtns";
 import NewJobBox from "./NewJobBox";
 import ColorGet from "./ColorGet";
+import { ModelJob } from "../model/ModelJob";
+
 function App() {  
   let getStyle = (alpha = 1)=>{
     let url = new URL(window.location.href);
     let colorIndex = parseInt(url.searchParams.get("colorIndex"))
     return `background:rgba(${ColorGet(colorIndex)},${alpha});`
   }
+
+  let getData = ()=>{
+    let year = parseInt(document.getElementById("year").innerHTML);
+    let month = parseInt(document.getElementById("month").innerHTML);
+    let date = parseInt(document.getElementById("date").innerHTML);
+    let hour0 = parseInt(document.getElementById("hour0").innerHTML);
+    let minute0 = parseInt(document.getElementById("minute0").innerHTML);
+    let hour1 = parseInt(document.getElementById("hour1").innerHTML);
+    let minute1 = parseInt(document.getElementById("minute1").innerHTML);
+    let job = new ModelJob();
+    job.StartTime = new Date(year,month-1,date,hour0,minute0,0,0).getTime();
+    job.EndTime = new Date(year,month-1,date,hour1,minute1,0,0).getTime();
+    job.RepeatType = 0;
+    job.AlertBefore = parseInt((document.getElementById("alertBefore") as HTMLInputElement).value);
+    job.JobInfo = (document.getElementById("jobInfo") as HTMLTextAreaElement).value;    
+    return job;
+  } 
+
   let save = async ()=>{
     let url = new URL(window.location.href);
-    // let data = NewJobBox.getData();
-    // data.ColorIndex = parseInt(url.searchParams.get("colorIndex"))
-    // let { ipcRenderer } = require("electron");
-    // await ipcRenderer.invoke("saveToDb","Job",data);
+    let data = getData();
+    data.ColorIndex = parseInt(url.searchParams.get("colorIndex"))
+    let { ipcRenderer } = require("electron");
+    await ipcRenderer.invoke("saveToDb","Job",data);
   }
   return (
     <>
--- render/NewJobBox.tsx
+++ render/NewJobBox.tsx
@@ -1,33 +1,16 @@
 import React from "./React";
 import "./NewJobBox.scss";
-import { ModelJob } from "../model/ModelJob";
 
 export default function() {
-  let getData = ()=>{
-    let year = parseInt(document.getElementById("year").innerHTML);
-    let month = parseInt(document.getElementById("month").innerHTML);
-    let date = parseInt(document.getElementById("date").innerHTML);
-    let hour0 = parseInt(document.getElementById("hour0").innerHTML);
-    let minute0 = parseInt(document.getElementById("minute0").innerHTML);
-    let hour1 = parseInt(document.getElementById("hour1").innerHTML);
-    let minute1 = parseInt(document.getElementById("minute1").innerHTML);
-    
-    let job = new ModelJob();
-    job.StartTime = new Date(year,month-1,date,hour0,minute0,0,0).getTime();
-    job.EndTime = new Date(year,month-1,date,hour1,minute1,0,0).getTime();
-    job.RepeatType = 0;
-    job.AlertBefore = parseInt((document.getElementById("alertBefore") as HTMLInputElement).value);
-    job.JobInfo = (document.getElementById("jobInfo") as HTMLTextAreaElement).value;    
-    return job;
-  } 
   document.addEventListener("DOMContentLoaded", async ()=>{
     let url = new URL(window.location.href)
     let startTime = new Date(parseInt(url.searchParams.get("startTime")))
-    document.getElementById("year").innerHTML = startTime.getFullYear().toString();
-    document.getElementById("month").innerHTML = (startTime.getMonth()+1).toString();
-    document.getElementById("date").innerHTML = (startTime.getDate()).toString();
-    document.getElementById("hour0").innerHTML = (startTime.getHours()).toString();
-    document.getElementById("hour1").innerHTML = (startTime.getHours()+1).toString();
+    let timeBox = document.getElementById("NewJobBox").firstElementChild;
+    timeBox.querySelector("#year").innerHTML = startTime.getFullYear().toString();
+    timeBox.querySelector("#month").innerHTML = (startTime.getMonth()+1).toString();
+    timeBox.querySelector("#date").innerHTML = (startTime.getDate()).toString();
+    timeBox.querySelector("#hour0").innerHTML = (startTime.getHours()).toString();
+    timeBox.querySelector("#hour1").innerHTML = (startTime.getHours()+1).toString();
   })
   return <div id="NewJobBox">
   <div class="timeBox">
