4e969308cceaef04dc1cfc4048dbb21409417119
unknown <liuxiaolun@hikvision.com>
2023-11-21 07:55:11 +0800

    month view job title

--- render/ViewMonth.tsx
+++ render/ViewMonth.tsx
@@ -3,34 +3,43 @@ import { dataMonth } from "./DataMonth";
 import React from "./React";
 import "./ViewMonth.scss";
 export default function () {
-    eventer.on("dataReady",()=>{
-        let container = document.getElementById("ViewMonth").lastElementChild;
-        let index = 0;
-        for(let i=0;i<6;i++){
-            let row = <div class="row"></div>            
-            for(let j=0;j<7;j++){
-                let dayObj = dataMonth.dateArr[index];
-                let cell = <div><div class="cellHeader">{dayObj.day}</div></div>
-                if(!dayObj.isCurMonth){
-                    cell.classList.add("notCurMonth");
-                }
-                if(dayObj.isCurDay){
-                    cell.classList.add("selected");
-                }
-                let cellContent = <div class="cellContent"></div>
-                dayObj.jobs.forEach(job=>{
-                    cellContent.appendChild(<div class="cellItem">{job.JobInfo}</div>)
-                })
-                cell.appendChild(cellContent)
-                row.appendChild(cell)
-                index += 1;
-            }
-            container.append(row)
+  eventer.on("dataReady", () => {
+    let container = document.getElementById("ViewMonth").lastElementChild;
+    let index = 0;
+    for (let i = 0; i < 6; i++) {
+      let row = <div class="row"></div>;
+      for (let j = 0; j < 7; j++) {
+        let dayObj = dataMonth.dateArr[index];
+        let cell = (
+          <div>
+            <div class="cellHeader">{dayObj.day}</div>
+          </div>
+        );
+        if (!dayObj.isCurMonth) {
+          cell.classList.add("notCurMonth");
         }
-    })
+        if (dayObj.isCurDay) {
+          cell.classList.add("selected");
+        }
+        let cellContent = <div class="cellContent"></div>;
+        dayObj.jobs.forEach((job) => {
+          cellContent.appendChild(
+            <div class="cellItem" title={job.JobInfo}>
+              {job.JobInfo}
+            </div>
+          );
+        });
+        cell.appendChild(cellContent);
+        row.appendChild(cell);
+        index += 1;
+      }
+      container.append(row);
+    }
+  });
 
-    return <div id="ViewMonth" class="view">
-        <div class="row header">
+  return (
+    <div id="ViewMonth" class="view">
+      <div class="row header">
         <div>一</div>
         <div>二</div>
         <div>三</div>
@@ -38,7 +47,8 @@ export default function () {
         <div>五</div>
         <div>六</div>
         <div>日</div>
+      </div>
+      <div class="content"></div>
     </div>
-    <div class="content"></div> 
-    </div>
+  );
 }
