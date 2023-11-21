ce2f4acf5da467db4efb1456071dff6da9d0a72b
unknown <liuxiaolun@hikvision.com>
2023-11-21 13:54:38 +0800

    switch btn dataReady

--- render/SwitchBtns.tsx
+++ render/SwitchBtns.tsx
@@ -1,37 +1,50 @@
+import { eventer } from "../common/eventer";
+import { dataSetting } from "./DataSetting";
 import React from "./React";
 import "./SwitchBtns.scss";
 export default function () {
-  let getId = (dom)=>{
-    if(dom.innerHTML === "日"){
-      return "ViewDay"
-    }else if(dom.innerHTML === "周"){
-      return "ViewWeek"
-    }else{
-      return "ViewMonth"
+  let switchBtnClick = (e) => {
+    let target = e.target as HTMLElement;
+    if (target.classList.contains("selected")) return;
+    let selectedDom = target.parentElement.querySelector(".selected") as HTMLElement;
+    if (selectedDom) {
+      selectedDom.classList.remove("selected");
     }
-  }
-  let switchBtnClick = (e)=>{
-    let target = e.target as HTMLElement
-    if(target.classList.contains("selected")) return;
-    let selectedDom = target.parentElement.querySelector(".selected") as HTMLElement
-    selectedDom.classList.remove("selected")
-    let id = getId(selectedDom)
-    document.getElementById(id).style.zIndex = "0"
-    target.classList.add("selected")
-    id = getId(target);
-    document.getElementById(id).style.zIndex = "20"
-  }
+    let arr = ["ViewDay", "ViewWeek", "ViewMonth"];
+    arr.forEach((v) => {
+      document.getElementById(v).style.zIndex = "0";
+    });
+    target.classList.add("selected");
+    let index = 0;
+    if (target.innerHTML === "周") {
+      index = 1;
+    } else if (target.innerHTML === "月") {
+      index = 2;
+    }
+    document.getElementById(arr[index]).style.zIndex = "20";
+  };
+  eventer.once("dataReady", () => {
+    let index = dataSetting.setting.ViewDefault;
+    let dom = document.getElementById("SwitchBtns").firstElementChild;
+    switchBtnClick({ target: dom.children[index] });
+  });
 
-  return <div id="SwitchBtns">
-    <div class="btns navigateBtns" onClick={switchBtnClick}>
-        <div class="selected">日</div>
+  return (
+    <div id="SwitchBtns">
+      <div class="btns navigateBtns" onClick={switchBtnClick}>
+        <div>日</div>
         <div>周</div>
         <div>月</div>
-    </div>
-    <div class="btns dayBtns">
-        <div><i class="iconfont icon-zuojiantou"></i></div>
+      </div>
+      <div class="btns dayBtns">
+        <div>
+          <i class="iconfont icon-zuojiantou"></i>
+        </div>
         <div>2011-11-11</div>
-        <div><i class="iconfont icon-youjiantou"></i></div>
+        <div>
+          <i class="iconfont icon-youjiantou"></i>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
