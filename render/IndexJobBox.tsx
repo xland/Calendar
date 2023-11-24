d24494534309e1f3ad0e11a338eee779b34289f7
unknown <412588801@qq.com>
2023-11-24 15:46:09 +0800

    aa

--- render/IndexJobBox.tsx
+++ render/IndexJobBox.tsx
@@ -6,18 +6,35 @@ export default function () {
   let loaded = (e: CustomEvent) => {
     let job = e.detail as ModelJob;
     let startTime = new Date(job.StartTime);
-    let timeBox = document.getElementById("IndexJobBox").firstElementChild;
-    timeBox.querySelector("#year").innerHTML = startTime.getFullYear().toString();
-    timeBox.querySelector("#month").innerHTML = (startTime.getMonth() + 1).toString();
-    timeBox.querySelector("#date").innerHTML = startTime.getDate().toString();
-    timeBox.querySelector("#hour0").innerHTML = startTime.getHours().toString();
-    timeBox.querySelector("#hour1").innerHTML = (startTime.getHours() + 1).toString();
+    let endTime = new Date(job.EndTime);
+    let dom = document.getElementById("IndexJobBox").firstElementChild.firstElementChild as HTMLElement;
+    dom.innerHTML = startTime.getFullYear().toString() + "年";
+    dom = dom.nextElementSibling as HTMLElement;
+    dom.innerHTML = (startTime.getMonth() + 1).toString() + "月";
+    dom = dom.nextElementSibling as HTMLElement;
+    dom.innerHTML = startTime.getDate().toString() + "日 &nbsp;";
+    dom = dom.nextElementSibling as HTMLElement;
+    dom.innerHTML = startTime.getHours().toString().padStart(2, "0") + ":";
+    dom = dom.nextElementSibling as HTMLElement;
+    dom.innerHTML = startTime.getMinutes().toString().padStart(2, "0") + "——";
+    dom = dom.nextElementSibling as HTMLElement;
+    dom.innerHTML = endTime.getHours().toString().padStart(2, "0") + ":";
+    dom = dom.nextElementSibling as HTMLElement;
+    dom.innerHTML = endTime.getMinutes().toString().padStart(2, "0");
     let taEle = document.getElementById("jobInfo") as HTMLTextAreaElement;
     taEle.value = job.JobInfo;
     let inputId = taEle.nextElementSibling as HTMLInputElement;
     inputId.value = job.Id;
-
-    let year = startTime.getFullYear();
+  };
+  let timeBtnClick = (e: MouseEvent) => {
+    let target = e.currentTarget as HTMLElement;
+    target = target.nextElementSibling as HTMLElement;
+    target.style.display = "block";
+    target.focus();
+  };
+  let selectOptionBlur = (e: Event) => {
+    let target = e.currentTarget as HTMLElement;
+    target.style.display = "none";
   };
   let yearOptionMouseWheel = (e: WheelEvent) => {
     let target = e.currentTarget as HTMLElement;
@@ -70,36 +87,75 @@ export default function () {
   };
   return (
     <div id="IndexJobBox" onLoaded={loaded}>
-      <div class="timeBox">
-        <div>
-          <div class="rowItem" onMouseWheel={yearOptionMouseWheel} onClick={yearClick}>
-            <span id="year"></span>年
-          </div>
-          <div tabindex="-1" class="optionBox" onMouseWheel={yearOptionMouseWheel} onBlur={optionBoxBlur}></div>
+      <div class="selectBtn" onClick={timeBtnClick}>
+        <span id="year"></span>
+        <span id="month"></span>
+        <span id="date"></span>
+        <span id="hour0"></span>
+        <span id="minute0">00</span>
+        <span id="hour1"></span>
+        <span id="minute1">00</span>
+      </div>
+      <div class="selectOption" tabindex="-1" onBlur={selectOptionBlur}>
+        <div class="selectColumn" style="width:52px">
+          <div>2023年</div>
+          <div>2023年</div>
+          <div class="selectCell">2023年</div>
+          <div>2023年</div>
+          <div>2023年</div>
         </div>
-        <div>
-          <span id="month"></span>月
+        <div class="selectColumn">
+          <div>1月</div>
+          <div>1月</div>
+          <div class="selectCell">1月</div>
+          <div>1月</div>
+          <div>1月</div>
         </div>
-        <div>
-          <span id="date"></span>日
+        <div class="selectColumn">
+          <div>22日</div>
+          <div>23日</div>
+          <div class="selectCell">23日</div>
+          <div>23日</div>
+          <div>23日</div>
         </div>
-        <div style="margin-left:3px">
-          <span id="hour0"></span>时
+        <div class="selectColumn">
+          <div>06时</div>
+          <div>06时</div>
+          <div class="selectCell">06时</div>
+          <div>06时</div>
+          <div>06时</div>
         </div>
-        <div style="margin-right:3px">
-          <span id="minute0">00</span>分
+        <div class="selectColumn">
+          <div>33分</div>
+          <div>33分</div>
+          <div class="selectCell">33分</div>
+          <div>33分</div>
+          <div>33分</div>
         </div>
-        ——
-        <div style="margin-left:3px">
-          <span id="hour1"></span>时
+        <div class="selectColumn selectColumnSpan">
+          <div>-</div>
+          <div>-</div>
+          <div class="selectCell">-</div>
+          <div>-</div>
+          <div>-</div>
         </div>
-        <div>
-          <span id="minute1">00</span>分
+        <div class="selectColumn">
+          <div>06时</div>
+          <div>06时</div>
+          <div class="selectCell">06时</div>
+          <div>06时</div>
+          <div>06时</div>
+        </div>
+        <div class="selectColumn">
+          <div>33分</div>
+          <div>33分</div>
+          <div class="selectCell">33分</div>
+          <div>33分</div>
+          <div>33分</div>
         </div>
       </div>
-      <div>
-        <div class="recycleBtn">不重复</div>
-      </div>
+      <div class="selectBtn">不重复</div>
+      <div class="selectOption"></div>
       <div class="textareaBox">
         <textarea id="jobInfo" spellCheck={false} placeholder="事件内容"></textarea>
         <input id="jobId" type="hidden" />
