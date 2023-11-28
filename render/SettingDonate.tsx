6c79b9194c17202e9720ebc77baa22cd334a211d
unknown <412588801@qq.com>
2023-11-28 15:47:25 +0800

    Update SettingDonate.tsx

--- render/SettingDonate.tsx
+++ render/SettingDonate.tsx
@@ -5,11 +5,11 @@ export default function (props) {
     <div class="settingBox" id="settingDonate">
       <div>
         <img src="./res/wechat.png" alt="" />
-        <div>微信支付</div>
+        <div>微信</div>
       </div>
       <div>
         <img src="./res/alipay.jpg" alt="" />
-        <div>支付宝支付</div>
+        <div>支付宝</div>
       </div>
     </div>
   );
