220bcf3359a0fb2941b31fcc03d1f30f3bc9074f
unknown <412588801@qq.com>
2023-12-01 10:00:19 +0800

    recent jobs

--- render/RecentJob.tsx
+++ render/RecentJob.tsx
@@ -7,11 +7,31 @@ export default function () {
     let { ipcRenderer } = require("electron");
     let jobs: ModelJob[] = await ipcRenderer.invoke("getDataRecent");
     let dom = document.getElementById("recentJobBox");
+    let getStartTimeStr = (startTime: number) => {
+      let span = startTime - Date.now();
+      if (span < 3600000) {
+        //1个小时
+        return `${Math.floor(span / 3600000)}分钟后`;
+      } else if (span < 86400000) {
+        //1天
+        return `${Math.floor(span / 3600000)}小时后`;
+      } else {
+        let startObj = new Date(startTime);
+        let now = new Date();
+        if (startObj.getMonth() === now.getMonth() && startObj.getFullYear() === now.getFullYear()) {
+          return `${startObj.getDate() - now.getDate()}天后`;
+        } else if (startObj.getFullYear() === now.getFullYear()) {
+          return `${startObj.getMonth() + 1}月${startObj.getDate()}日`;
+        } else {
+          return `${startObj.getFullYear()}-${startObj.getMonth() + 1}-${startObj.getDate()}`;
+        }
+      }
+    };
     for (let i = 0; i < jobs.length; i++) {
       dom.append(
         <div class="item">
           <div class="title">{jobs[i].JobInfo}</div>
-          <div class="time">3小时后</div>
+          <div class="time">{getStartTimeStr(jobs[i].StartTime)}</div>
         </div>
       );
     }
