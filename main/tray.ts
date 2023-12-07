609984254f3d26f75b923c144c0c6e49d951ec7e
unknown <412588801@qq.com>
2023-12-07 20:35:17 +0800

    Update tray.ts

--- main/tray.ts
+++ main/tray.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { Tray as TrayObj, Menu, app } from "electron";
 import { main } from "./main";
 class Tray {
@@ -9,7 +10,7 @@ class Tray {
     app.exit();
   }
   async init() {
-    this.tray = new TrayObj("./res/icon.png");
+    this.tray = new TrayObj(path.join(__dirname, "res/icon.png"));
     let contextMenu = Menu.buildFromTemplate([
       {
         label: "退出",
