843577656d9efeadac8cf02e6417ab71f252e127
unknown <412588801@qq.com>
2023-12-07 10:57:21 +0800

    Update RecentJob.tsx

--- render/RecentJob.tsx
+++ render/RecentJob.tsx
@@ -4,12 +4,24 @@ import { eventer } from "../common/eventer";
 import { ModelJob } from "../model/ModelJob";
 import { Helper } from "../common/Helper";
 export default function () {
-  eventer.on("dataReady", async () => {
+  let jobs: ModelJob[];
+  let timeOutId;
+  let initAlert = (job: ModelJob) => {
+    timeOutId = setTimeout(async () => {
+      console.log(job);
+      await initData();
+    }, job.StartTime - Date.now() + 1);
+  };
+  let initData = async () => {
+    clearTimeout(timeOutId);
     let { ipcRenderer } = require("electron");
-    let jobs: ModelJob[] = await ipcRenderer.invoke("getDataRecent");
+    jobs = await ipcRenderer.invoke("getDataRecent");
     let dom = Helper.$id("RecentJob");
     dom.innerHTML = "";
     for (let i = 0; i < jobs.length; i++) {
+      if (i === 0) {
+        initAlert(jobs[i]);
+      }
       dom.append(
         <div class="item" data-id={jobs[i].Id} data-start={jobs[i].StartTime} onMouseDown={Helper.jobMouseDown} onDblClick={Helper.jobDbClick}>
           <div class="title">{jobs[i].JobInfo}</div>
@@ -17,10 +29,9 @@ export default function () {
         </div>
       );
     }
-    let firstJob = jobs[0];
-    setTimeout(() => {
-      alert(123);
-    }, firstJob.StartTime - Date.now());
+  };
+  eventer.on("dataReady", async () => {
+    await initData();
   });
 
   return <div id="RecentJob" class="jobList"></div>;
