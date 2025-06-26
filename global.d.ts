37447b860580d94e212e9dbf308189e0dbeac899
xland <412588801@qq.com>
2025-06-26 17:21:23 +0800

    db

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,67 @@
+type BaseResult = {
+  /**
+   * 操作是否成功
+   */
+  ok: boolean;
+  /**
+   * 如果操作不成功，err将携带错误信息
+   */
+  err?: string;
+};
+
+type Result<T = void> = BaseResult & T;
+
+type WinEventName = "maximize" | "minimize" | "close"; //todo
+
 interface Win {
-  startDrag: () => {};
-  on: (evtName: string, cb: (result: { state: string }) => void) => void;
-  minimize: () => void;
-  restore: () => void;
-  maximize: () => void;
-  close: () => void;
+  /**
+   *鼠标按下，开始拖拽移动窗口的位置
+   */
+  startDrag(): Result;
+  /**
+   * 监听窗口事件
+   * @param evtName 事件名称
+   * @param cb 事件发生时的回调函数
+   */
+  on(
+    evtName: string,
+    cb: (result: {
+      /**
+       * 窗口状态
+       */
+      state: string;
+    }) => void
+  ): Result;
+  minimize(): Result;
+  restore(): Result;
+  /**
+   *
+   */
+  maximize(): Result;
+  /**
+   * 关闭窗口
+   * @returns
+   */
+  close(): Result;
 }
 interface Db {
-  open(dbPath: string, inDataPath?: boolean): { ok: boolean; isDbFirstCreated: boolean; data: string; err?: string };
+  /**
+   *
+   * @param dbPath
+   * @param inDataPath
+   */
+  open(
+    dbPath: string,
+    inDataPath?: boolean
+  ): Result<{
+    /**
+     * 是否第一次创建数据库
+     */
+    isDbFirstCreated: boolean;
+    data: string;
+  }>;
+
+  sql(sql: string, dbPath?: string): Result;
 }
 interface Horse {
   win: Win;
