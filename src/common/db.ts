8dc396b5b467ffc6e81967dd046e78debd4b2ca3
xland <412588801@qq.com>
2025-06-27 17:22:15 +0800

    Update db.ts

--- src/common/db.ts
+++ src/common/db.ts
@@ -4,12 +4,10 @@ class Db {
   promiser: Promiser;
   constructor() {}
   async open() {
-    debugger;
     this.promiser = await new Promise((resolve) => {
       const _promiser = sqlite3Worker1Promiser({
         worker: () => {
           let url = new URL(location.href);
-          debugger;
           return new Worker(new URL("sqlite3-worker1-bundler-friendly.mjs", url.origin), {
             type: "module",
           });
@@ -21,13 +19,39 @@ class Db {
       filename: "file:db.sqlite3?vfs=opfs",
     });
     this.id = openResponse.dbId;
-    await this.promiser("exec", {
+    const checkTable = await this.promiser("exec", {
       dbId: this.id,
-      sql: `CREATE TABLE Job(Id VARCHAR2(36) NOT NULL ON CONFLICT FAIL UNIQUE ON CONFLICT FAIL,JobInfo TEXT,RepeatType INT,StartTime BIGINT,EndTime BIGINT,ColorIndex INT);
-CREATE INDEX JobInfo_Index ON Job(JobInfo);
-CREATE TABLE Setting(ViewDefault INT DEFAULT 0,ViewVal Int,LangDefault INT DEFAULT 0,SkinDefault INT DEFAULT 0,AlertBefore INT);
-INSERT INTO Setting (ViewDefault,ViewVal,LangDefault,SkinDefault,AlertBefore) VALUES (0,0,0,0,5);`,
+      sql: `SELECT name FROM sqlite_master WHERE type='table' AND name='Job';`,
+      callback: (result) => {
+        console.log(result);
+      },
     });
+    debugger;
+    if (1 === 0) {
+      // 表不存在 => 是第一次创建
+      await this.promiser("exec", {
+        dbId: this.id,
+        sql: `
+      CREATE TABLE Job(
+        Id VARCHAR2(36) NOT NULL PRIMARY KEY,
+        JobInfo TEXT,
+        RepeatType INT,
+        StartTime BIGINT,
+        EndTime BIGINT,
+        ColorIndex INT
+      );
+      CREATE INDEX JobInfo_Index ON Job(JobInfo);
+      CREATE TABLE Setting(
+        ViewDefault INT DEFAULT 0,
+        ViewVal INT,
+        LangDefault INT DEFAULT 0,
+        SkinDefault INT DEFAULT 0,
+        AlertBefore INT
+      );
+      INSERT INTO Setting (ViewDefault, ViewVal, LangDefault, SkinDefault, AlertBefore)
+      VALUES (0, 0, 0, 0, 5);`,
+      });
+    }
   }
 }
 export let db = new Db();
