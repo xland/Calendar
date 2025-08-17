151a3516461f5b0da0c031bde221c6bf7b796f8b
liulun <xland@live.cn>
2025-08-17 17:18:57 +0800

    btns

--- Models/DateModel.cs
+++ Models/DateModel.cs
@@ -1,6 +1,15 @@
+using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace Calendar.Models;
 
-public class DateModel
+public partial class DateModel: ObservableObject
 {
-    
+    [ObservableProperty]
+    private int _date;
+    [ObservableProperty]
+    private string _lunar = String.Empty;
+    [ObservableProperty]
+    private IRelayCommand _command = null!;
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace Calendar.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using Calendar.Models;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Calendar.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public ObservableCollection<DateModel> Buttons { get; }
+    public MainWindowViewModel()
+    {
+        Buttons = new ObservableCollection<DateModel>();
+
+        for (int i = 0; i < 42; i++) // 6×7
+        {
+            int index = i;
+            Buttons.Add(new DateModel
+            {
+                Date = index,
+                Lunar = "测试",
+                Command = new RelayCommand(() =>
+                {
+                    Console.WriteLine($"Button {index + 1} clicked");
+                })
+            });
+        }
+    }
 }
