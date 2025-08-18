7725f91b8aa4a371d5338c56f5014af59af7cc55
xland <412588801@qq.com>
2025-08-18 16:56:56 +0800

    doc

--- Models/DateModel.cs
+++ Models/DateModel.cs
@@ -7,9 +7,15 @@ namespace Calendar.Models;
 public partial class DateModel: ObservableObject
 {
     [ObservableProperty]
-    private int _date;
+    private DateOnly _date;
     [ObservableProperty]
     private string _lunar = String.Empty;
     [ObservableProperty]
     private IRelayCommand _command = null!;
+    [ObservableProperty]
+    private bool _isCurMonth = false;
+    [ObservableProperty]
+    private bool _isToday = false;
+    [ObservableProperty]
+    private bool _hasSchedule = false;
 }
--- Views/Header.axaml.cs
+++ Views/Header.axaml.cs
@@ -1,6 +1,10 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using Calendar.ViewModels;
 
 namespace Calendar.Views;
 
@@ -9,5 +13,31 @@ public partial class Header : UserControl
     public Header()
     {
         InitializeComponent();
+        this.PointerPressed += OnHeaderPointerPressed;
+    }
+    private void OnHeaderPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            return;
+        }
+        var window = this.FindAncestorOfType<Window>();
+        window?.BeginMoveDrag(e);
+    }
+
+    private void OnPreBtnClick(object? sender, RoutedEventArgs e)
+    {
+        if (this.DataContext is MainWindowViewModel parentVm)
+        {
+            parentVm.GoPreMonth();
+        }
+    }
+
+    private void OnNextBtnClick(object? sender, RoutedEventArgs e)
+    {
+        if (this.DataContext is MainWindowViewModel parentVm)
+        {
+            parentVm.GoNextMonth();
+        }
     }
 }
