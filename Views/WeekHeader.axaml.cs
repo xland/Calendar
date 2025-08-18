745dff867caa517131ff06fed8b9acb86378bb96
xland <412588801@qq.com>
2025-08-18 10:28:01 +0800

    header icon font

---
+++ Views/WeekHeader.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Calendar.Views;
+
+public partial class WeekHeader : UserControl
+{
+    public WeekHeader()
+    {
+        InitializeComponent();
+    }
+}
